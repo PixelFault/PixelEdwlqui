77821bf3073bfc1b5e37216e67ab00f71ddf8d19
PixelFault <luca.georges-francois@epitech.eu>
2020-09-02 22:30:44 +0200

    [+] - Material UI libs and upload button which opens file explorer.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import UploadButton from "./components/UploadButton";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <p>
           PixelEdwlqui
         </p>
+        <UploadButton text="Upload" variant="outlined" color="secondary"/>
       </header>
     </div>
   );
